1c841d7b302fa10c0f868350ae9fac8b70215a33
shifra-fraiman <s0548549680@gmail.com>
2024-06-10 15:56:55 +0300

    add body-parser for params request

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ import businessRouter from "./routes/business.routes";
 const app = express();
 const port = 3000;
 
-// app.use(express.json());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use("/business", businessRouter);
 
 app.listen(port, () => {
