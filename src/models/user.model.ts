7487b16bbf97f6fda46cb2b947d41a4124fd36f1
shifra-fraiman <s0548549680@gmail.com>
2024-09-11 16:01:53 +0300

    extends Document

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,6 +1,6 @@
-import {Schema, model} from "mongoose";
+import {Schema,Document, model} from "mongoose";
 
-export interface User {
+export interface User extends Document{
     userId: string;
     name: string;
     password: string;
